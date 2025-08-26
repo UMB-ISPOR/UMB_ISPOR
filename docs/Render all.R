a2114febdeb6a1bb270c08a04911e6f27f781fe4
lynnhuang-peilin <umbispor@gmail.com>
2025-08-26 03:08:26 -0400

    remove unnecessary files

--- docs/Render all.R
+++ docs/Render all.R
@@ -1,11 +1,3 @@
-
-rmarkdown::render("index.Rmd", output_format = "html_document", output_dir = "docs/")
-rmarkdown::render("ISPOR.Rmd", output_format = "html_document", output_dir = "docs/")
-rmarkdown::render("team.Rmd", output_format = "html_document", output_dir = "docs/")
-rmarkdown::render("table.Rmd", output_format = "html_document", output_dir = "docs/")
-rmarkdown::render("gallery.Rmd", output_format = "html_document", output_dir = "docs/")
-rmarkdown::render("follow.Rmd", output_format = "html_document", output_dir = "docs/")
-
 rmarkdown::render("index.Rmd", output_format = "html_document")
 rmarkdown::render("ISPOR.Rmd", output_format = "html_document")
 rmarkdown::render("team.Rmd", output_format = "html_document")
